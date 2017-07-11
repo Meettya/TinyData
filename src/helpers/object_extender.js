750a2ddee029ea2dd76b3911b621dbac9f85f326
Karpich Dmitry <karpich@gollard.ru>
2017-07-11 18:35:14 +0300

    * fix style

--- src/helpers/object_extender.js
+++ src/helpers/object_extender.js
@@ -3,19 +3,19 @@
  */
 import getItType from './type_detector'
 
-function doExtendObject(original, addon) {
+function doExtendObject (original, addon) {
   let addonType
   let result = {}
 
-  if(getItType(original) !== 'HASH'){
+  if (getItType(original) !== 'HASH') {
     throw TypeError(`must called with object, but get |${original}|`)
   }
-  if(getItType(addon) !== 'HASH'){
+  if (getItType(addon) !== 'HASH') {
     return original
   }
   Object.keys(original).forEach((key) => {
     addonType = getItType(addon[key])
-    if (!(addonType === 'NULL' || addonType === 'UNDEFINED')){
+    if (!(addonType === 'NULL' || addonType === 'UNDEFINED')) {
       result[key] = addon[key]
     } else {
       result[key] = original[key]
